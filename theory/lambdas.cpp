799a793be832111c9fba79b80004d27a038b7643
Thomas Mario <thmapneadesigner@gmail.com>
2024-08-29 18:53:28 +0200

    lambdas

---
+++ theory/lambdas.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+
+
+
+
+int main(){
+	//data
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	
+	//empty lambda with no parameters, no return type and no capture list
+	auto emptyLambda = [] () {
+		std::cout<<"my empty lambda"<<std::endl;
+	};
+	
+	//lambda that only uses parameters
+	auto parameterLambda = [] (int x, int y) {
+		std::cout<<x + y<<std::endl; 
+	};
+	
+	//lamda that uses parameters and capture list the value of the capture list must be declared in the same scope
+	auto captureListLambda = [c] (int x, int y) {
+		std::cout<<c * x * y<<std::endl;
+	};
+	
+	auto referenceLambda  = [&c] (int x) {
+		c += x;
+		std::cout<<c<<std::endl;
+	};
+		
+	
+	
+	//call of the lambdas function
+	emptyLambda();
+	parameterLambda(a,b);
+	captureListLambda(a,b); //as you can see we didn't need to pass the variable contained in the capture list to the lambda since the lambda takes it from it's scope
+	referenceLambda(a); //since we are passing the capture list per reference it will be updated automatically
+	return 0;
+}
