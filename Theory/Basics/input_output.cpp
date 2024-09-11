f6694ed5111da1f201455280233ba9d32c6891af
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-11 22:36:00 +0200

    input and output from keyboard

---
+++ Theory/Basics/input_output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+
+
+int main(){
+	int x;
+	std::cout<<"insert a integer: "<<std::endl;
+	//the cin keyword allows us to get input from the keyboard in the terminal and then in our program
+	//cin means character input and is treated in the buffer
+	std::cin>>x;
+	std::cout<<"the number is: "<<x<<std::endl;
+	
+	return 0;
+}
