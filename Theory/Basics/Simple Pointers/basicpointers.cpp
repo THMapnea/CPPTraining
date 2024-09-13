51b8c0da581bbb9cded8022b3f1e265c47895ab5
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-13 19:28:06 +0200

    Update basicpointers.cpp

--- Theory/Basics/Simple Pointers/basicpointers.cpp
+++ Theory/Basics/Simple Pointers/basicpointers.cpp
@@ -1,35 +1,34 @@
 #include<iostream>
 
+//we are maybe at the most complex topic for the basics of c++ the pointers
+
+
 
 
 //data
 int x = 10;
-int a = 2;
-int b;
 
-
-
+//before continuing to read remember about the (&) reference operator and the (*) dereference operator and keep in mind that in the moment of declaration
+//of a pointer (*) is not a dereference operator
 
 int main(){
-	//here we are declaring a pointer to an integer and assign it the address of the x variable
-	//int htis case the (*) operator indicates a pointer type variable and (&) is the reference operator
-	int *xpt = &x;
+	//we declare a pointer of int type that means that the variable pointed is of the int type
+	int *p; 
 	
-	//here we are using the dereferencing operator (*) to access the value pointed by the pointer and assigning it by doing *xpt	
-	std::cout<<"the address of the x variable is: "<<xpt<<" and it's value is: "<<*xpt<<std::endl;
+	//we assign to the pointer the address of the variable we want to point
+	p = &x;
 	
-	//this code help us understand that the value pointed changes but if we assign that value to one variable in runtime that one will not change since
-	//it's been executed only once
-	int *apt = &a;
-	b = *apt;
-	a = 7;
-	std::cout<<*apt<<","<<b<<","<<a<<std::endl;
+	//so p contains the address and *p means tell me the value that we can find at the address stored in p
+	std::cout<<"the address of the x variable is: "<<p<<" and the value pointed is: "<<*p<<std::endl; 
 	
-	//we also have different type of pointers
+	//trough the pointer we can also access that area of memory to perform operations like switching variables ecc
+	*p = 8,
 	
-	//null pointers are useful for comparison to make sure that the pointer we want to acces actually exist
-	int pt = nullptr; //this pointer is the only one that is equal to false when converted to bool
+	//as you will se now the address is the same but the value stored has been modified
+	std::cout<<"the address of the x variable is: "<<p<<" and the value pointed is: "<<*p<<std::endl; 
 	
-	return  0;
+	//we can also move ourself between address incrementing or decrementing pointers p++ means p = p + 1 
+	//so if for example p = 2000 then p++ = 2001 referrin to address and not variable
+	std::cout<<"the next memory address is: "<<p++<<std::endl;
+	return 0;
 }
-
