8fa9a961d7fd26e78def03f81aa3b70c17fc1945
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-16 11:26:38 +0200

    header file

---
+++ Theory/Intermediate/Code Splitting/header.h
@@ -0,0 +1,13 @@
+//this line is called guard and check to avoid double definition in the codebase
+#ifndef SOURCEFILE_H
+#define SOURCEFILE_H
+#include<iostream>
+
+struct Person{
+	int age;
+	std::string name;
+};
+
+
+//this is the end of the guarded block
+#endif
