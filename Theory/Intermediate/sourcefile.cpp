eeaf9e43c5a99fe08e58d8f170c3d86a3999f5a8
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-16 11:25:51 +0200

    sourcefile

---
+++ Theory/Intermediate/sourcefile.cpp
@@ -0,0 +1,13 @@
+//include the compiled header
+#include "header.h"
+#include<iostream>
+
+
+
+int main(){
+	//as you can see we can call the struct defined in our header file
+	Person p;
+	
+	
+	return 0;
+}
