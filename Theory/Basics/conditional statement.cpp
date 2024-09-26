b1c8cfc5163fd4ac2218e70b46ed5b6f784cb2b3
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-26 19:01:08 +0200

    Update conditional statement.cpp

--- Theory/Basics/conditional statement.cpp
+++ Theory/Basics/conditional statement.cpp
@@ -1,52 +1,45 @@
 #include<iostream>
 
-
-
+// Defining global boolean variables and an integer
 bool a = true;
 bool b = false;
 int c = 100;
 
+int main() {
+    // Checking if 'c' is greater than 10
+    if(c > 10) {
+        std::cout << "The number is bigger" << std::endl;
+    } else {
+        std::cout << "The number is smaller" << std::endl;
+    }
+    
+    // Chained if-else condition to handle multiple ranges of 'c'
+    if(c > 1000) {
+        std::cout << "The number c is humongous" << std::endl;	
+    } else if(c > 10) {
+        std::cout << "The number c is very big" << std::endl;
+    }
 
-
-
-int main(){
-	//to check for conditions we can use the if - else object
-	//for example if we want to see if one value is bigger than other we can do
-	if(c > 10){
-		std::cout<<"the number is bigger"<<std::endl;
-    }else{
-    	std::cout<<"the number is smaller"<<std::endl;
-	}
-	
-	//we can also concatenate if else
-	if(c > 1000){
-		std::cout<<"the number c is humongus"<<std::endl;	
-	}else if(c > 10){
-		std::cout<<"the number c is very big"<<std::endl;
-	}
-	
-	
-	//this is the same as writing if(a == true)
-	if(a){
-		std::cout<<" a is true"<<std::endl;
-	}
-	if(!b){
-		std::cout<<" b is false"<<std::endl;
-	}
-	
-	//even tough it'is not always a good practice you can nest if else statement
-	//but it's better to work around some boolean algebra to reduce this type of configuration that are prone to errors
-	if(a){
-		if(!b){
-			if(!a || !b){
-				if(a && !b){
-					std::cout<<"what a mess"<<std::endl;
-				}
-			}
-		}
-	}
-	
-	
-	
-	return 0;
+    // Simplified condition (same as if (a == true))
+    if(a) {
+        std::cout << "a is true" << std::endl;
+    }
+    
+    // Checking if 'b' is false (same as if (b == false))
+    if(!b) {
+        std::cout << "b is false" << std::endl;
+    }
+    
+    // Nested if-else statements (demonstrating the use of logical operators)
+    if(a) {                         // If 'a' is true
+        if(!b) {                    // And 'b' is false
+            if(!a || !b) {           // If 'a' is false or 'b' is false (in this case 'b' is false)
+                if(a && !b) {        // If 'a' is true and 'b' is false
+                    std::cout << "what a mess" << std::endl;
+                }
+            }
+        }
+    }
+    
+    return 0;
 }
