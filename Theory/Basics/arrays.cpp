a16deaad8aa7126cb976e12ce36bdef26afb045b
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-26 19:06:25 +0200

    Update arrays.cpp

--- Theory/Basics/arrays.cpp
+++ Theory/Basics/arrays.cpp
@@ -1,63 +1,53 @@
 #include<iostream>
 
-//const
-const int array_dim = 10;
+// Constants
+const int array_dim = 10;  // Constant dimension for the array
 
-//data
-int my_array[5] = {6,5,3,29,0};
-int my_no_dimension_array[] = {0,12,3,4,56,1,2,3,4,5,6,7,8}; //in this case the compiler will take care of giving the dimension to the array only if you initialize it's values
-int array[array_dim]; 
+// Data arrays
+int my_array[5] = {6, 5, 3, 29, 0};  // Fixed-size array with initialized values
+int my_no_dimension_array[] = {0, 12, 3, 4, 56, 1, 2, 3, 4, 5, 6, 7, 8};  // Compiler automatically determines the size
+int array[array_dim];  // Array with size defined by the constant
 
-//prototypes
-void show(int *array, int array_dim);
-void generate(int *array, int array_dim);
-void pointerAccessToArray(int *array, int array_dim);
+// Function prototypes
+void show(int* array, int array_dim);
+void generate(int* array, int array_dim);
+void pointerAccessToArray(int* array, int array_dim);
 
-
-
-int main(){
-	//the first thing that we want to do to an array is accessing it to do so we can use a for loop or a while loop depending on the situation
-	//where i will be our iterator that is used to access the i-th element of the array
-	for(int i = 0; i < 5; i++){
-		std::cout<<"the element "<<my_array[i]<<" is at index "<<i<<std::endl; //i am showing them but i can also perform action on it
-	}
-	
-	//from here on we will use function to make the code more clear if you dont' understand them go look up the function file
-	
-	//differently from c we dont need to use dereferenciation to pass the array to the function c++ does this for us
-	generate(array,array_dim);
-	show(array,array_dim);
-	pointerAccessToArray(array,array_dim);
-	return 0;
+int main() {
+    // Accessing and displaying the array using a for-loop
+    // where 'i' is the iterator to access the i-th element of the array
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Element " << my_array[i] << " is at index " << i << std::endl;
+    }
+    
+    // Generate values in the array and display them using functions
+    generate(array, array_dim);  // Filling the array with generated values
+    show(array, array_dim);  // Displaying the array content
+    pointerAccessToArray(array, array_dim);  // Accessing the array using pointer arithmetic
+    
+    return 0;
 }
 
-
-
-//we are passing to the function the pointer to our array to allows it to modify it since rememebr that all the element of the array are in consecutive memory space 
-void show(int *array, int array_dim){ //i am passing array_dim only to show since it'is in global scope it could be avoided
-	for(int i = 0; i < array_dim; i++){
-		std::cout<<"the array element is: "<<array[i]<<std::endl;
-	}
+// This function displays the contents of the array
+// The array is passed as a pointer, so it can modify or access the original memory space
+void show(int* array, int array_dim) {
+    for (int i = 0; i < array_dim; i++) {
+        std::cout << "Array element at index " << i << " is: " << array[i] << std::endl;
+    }
 }
 
-void generate(int *array, int array_dim){
-	for(int i = 0; i < array_dim; i++){
-		array[i] = i;
-	}
-	
+// This function fills the array with values based on its index (i.e., array[i] = i)
+void generate(int* array, int array_dim) {
+    for (int i = 0; i < array_dim; i++) {
+        array[i] = i;
+    }
 }
 
-//this example can be understood better by knowing pointer arytmethic we are basically deferencing our pointer in a way that we increase the address 
-//therefore passing to the next value due to how arrays are created
-void pointerAccessToArray(int *array, int array_dim){
-	for(int i = 0; i < array_dim; i++){
-		*(array + i) = i;
-		std::cout<<"the element of the array at the position: "<<i<<" accessed trough pointer arythmetic is: "<<*(array + i)<<std::endl;
-	}
+// This example demonstrates pointer arithmetic to access array elements.
+// Using the pointer, we increment the address to access each element in the array.
+void pointerAccessToArray(int* array, int array_dim) {
+    for (int i = 0; i < array_dim; i++) {
+        *(array + i) = i;  // Accessing and modifying the array using pointer arithmetic
+        std::cout << "Element at position " << i << " accessed through pointer arithmetic is: " << *(array + i) << std::endl;
+    }
 }
-
-
-
-
-
-
