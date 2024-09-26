f598dffb10accd9d58e28ba003d4f61bbcfd2fa4
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-26 18:56:27 +0200

    Update enumeration.cpp

--- Theory/Basics/enumeration.cpp
+++ Theory/Basics/enumeration.cpp
@@ -1,29 +1,41 @@
 #include<iostream>
 
+// The enumeration 'RGB' assigns values to red, green, and blue.
+// By default, 'red' is assigned the value 0, 'green' is 1, and 'blue' is 2.
+enum RGB { red, green, blue };
 
-//the enumeration gives us a way to store enumerated values with a name
-//that means that each value is given a number from 0 -> infinity all integer
-enum RGB {red,green,blue};
+// You can specify the starting value of an enumerator.
+// Here, 'cyan' starts with 2, 'purple' is 3, and 'yellow' is 4.
+enum CPY { cyan = 2, purple, yellow };
 
-//the enumerator can also be defined by you for example here instead of starting with 0 we start with two
-enum CPY {cyan = 2,purple,yellow};
+// You can use enum to implement boolean-like values.
+// This example creates a custom BOOL type where 'FALSE' is 0 and 'TRUE' is 1.
+// This is similar to a boolean type in C (before C++ added built-in bool types).
+typedef enum BOOL { FALSE, TRUE } BOOL;
 
-//this is how you can implement boolean values in c for example you should also use typedef but we will see it later how it works
-typedef enum BOOL {FALSE,TRUE};
+int main() {
+    // Declare a variable 'r' of the RGB enum type and assign it the value 'red'.
+    RGB r = red;
+    
+    // Declare a variable 'p' of the CPY enum type and assign it the value 'purple'.
+    CPY p = purple;
+    
+    // Output the values of the enumerators. 
+    // 'r' corresponds to the value of 'red' (0), and 'p' corresponds to 'purple' (3).
+    std::cout << "The number associated with red is: " << r << std::endl;
+    std::cout << "The number associated with purple is: " << p << std::endl;
+    
+    // Using the custom BOOL type to evaluate a condition.
+    // Since 'TRUE' is defined as 1, this if statement will execute.
+    if (TRUE) {
+        std::cout << "The value is true" << std::endl;
+    }
 
-//you can also implement other type of enumeration but those are more complex and less useful at the beginner level
-int main(){
-	//we first write the name of the enum then the variable name and in the end the coonstant value of our enumeration that we want to assign to it
-	RGB r = red;
-	CPY p = purple;
-	std::cout<<"the number associated to the color is: "<<r<<" and the number associated to the other color is: "<<p<<std::endl;
-	if(TRUE){
-		std::cout<<"the value is true"<<std::endl;
-	}
-	if(!FALSE){
-		std::cout<<"the value is false"<<std::endl;
-	}
-	
-	
-	return 0;
+    // Since 'FALSE' is defined as 0, this condition evaluates as false (!0 = true).
+    // Therefore, this block will also execute.
+    if (!FALSE) {
+        std::cout << "The value is false" << std::endl;
+    }
+    
+    return 0;
 }
