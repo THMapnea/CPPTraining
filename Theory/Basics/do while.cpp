3bc06a21561887a546ace56d09d276f8171294b1
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-26 18:57:17 +0200

    Update do while.cpp

--- Theory/Basics/do while.cpp
+++ Theory/Basics/do while.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
 
-
-
-
-
-
-int main(){
-	
-	int i = 0; //initialize a counter
-	
-	//differently from the while the do while check the condition after the execution of the block
-	//it can be useful if we want to verify a condition before incrementing a value 
-	do{
-		std::cout<<"iteration number: "<<i++<<std::endl;
-	}while(i < 10);
-	
-	return 0;
+int main() {
+    int i = 0; // Initialize a counter
+    
+    // The 'do-while' loop always executes the block of code at least once.
+    // This is different from a regular 'while' loop, where the condition is checked before the first iteration.
+    // In a 'do-while' loop, the condition is checked after the block is executed.
+    do {
+        std::cout << "Iteration number: " << i++ << std::endl;
+    } while (i < 10);  // The loop will continue until 'i' is less than 10.
+    
+    return 0;
 }
