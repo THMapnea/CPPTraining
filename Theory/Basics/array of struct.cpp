f532de4c5f99ece0a7ade76b15cad7c02cffbbd5
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-27 20:36:06 +0200

    Update array of struct.cpp

--- Theory/Basics/array of struct.cpp
+++ Theory/Basics/array of struct.cpp
@@ -1,43 +1,47 @@
 #include<iostream>
 
-
-struct Contact{
-	int age;
-	bool alive;
+// Defining a simple struct to represent a contact
+struct Contact {
+    int age;
+    bool alive;
 };
 
-
-//let's try to use multiple data structure togheter
+// Declaring an array of structs to hold multiple contacts (10 elements)
 struct Contact list_of_contact[10];
 
-//prototype
-void fill(struct Contact *lc);
-void show(struct Contact *lc);
+// Function prototypes
+void fill(struct Contact* lc);
+void show(struct Contact* lc);
 
-int main(){
-	fill(list_of_contact);
-	show(list_of_contact);
-	
-	
-	return 0;
+int main() {
+    // Filling the array with data
+    fill(list_of_contact);
+
+    // Displaying the contents of the array
+    show(list_of_contact);
+
+    return 0;
 }
 
-//here we fill the list of struct !!we need to use the pointer notation to tell the function that the parameter is a pointer to an array
-void fill(struct Contact *lc){
-	for(int i = 0; i < 10; i++){
-		lc[i].age = i; //we can easily combine struct notation and array notation 
-		lc[i].alive  = true;//we are basically saying take the i-th element and get its (.) property and modify it
-	}
+// This function fills the array of structs with data.
+// The argument is a pointer to the first element of the array of structs.
+void fill(struct Contact* lc) {
+    for (int i = 0; i < 10; i++) {
+        lc[i].age = i;  // Assigning values to the 'age' field of the struct
+        lc[i].alive = true;  // Setting the 'alive' field to true
+    }
 }
 
-//function that shows the output
-void show(struct Contact *lc){
-	for(int i  = 0; i < 10; i++){
-		std::cout<<"the "<<i<<" contact is "<<lc[i].age;
-		if(lc[i].alive){
-			std::cout<<" and is alive"<<std::endl;
-		}else{
-			std::cout<<" and is dead"<<std::endl;
-		}
-	}
+// This function displays the contents of the array of structs.
+// The argument is a pointer to the array.
+void show(struct Contact* lc) {
+    for (int i = 0; i < 10; i++) {
+        // Accessing and displaying the 'age' and 'alive' fields of each struct
+        std::cout << "The contact " << i << " is " << lc[i].age << " years old";
+        if (lc[i].alive) {
+            std::cout << " and is alive." << std::endl;
+        } else {
+            std::cout << " and is dead." << std::endl;
+        }
+    }
 }
