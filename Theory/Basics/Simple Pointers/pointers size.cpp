032d0730e13d940ebb089d53d243775e701d005a
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-27 20:50:39 +0200

    Update pointers size.cpp

--- Theory/Basics/Simple Pointers/pointers size.cpp
+++ Theory/Basics/Simple Pointers/pointers size.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
 
+// We will now see a little bit of pointer arithmetic and how to use the `sizeof` function with pointers and values
 
-
-//we will now see a little bit of pointer arythmetics
-
-
-//data
+// Data: A simple integer variable
 int a = 1002;
 
+int main() {
+    // Declare a pointer `p` and assign it the address of the variable `a`
+    int *p = &a;
+    
+    // `sizeof(p)` returns the size of the pointer itself (usually 8 bytes on 64-bit systems)
+    // `sizeof(*p)` returns the size of the data type the pointer is pointing to (in this case, an `int`, which is typically 4 bytes)
+    std::cout << "The size of the pointer is " << sizeof(p)
+              << " bytes and the size of the pointed value (int) is: " << sizeof(*p) << " bytes" << std::endl;
 
-int main(){
-	int *p  = &a;
-	
-	//we can also check the size of our pointed variable using sizeof() method
-	std::cout<<"the size of the pointer is "<<sizeof(p)<<" and the size of the pointed value is: "<<sizeof(*p)<<std::endl;
- 
-	return 0;
+    return 0;
 }
