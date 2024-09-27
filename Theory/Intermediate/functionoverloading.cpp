0129ce66b413cf7430141e438372b52d302da5ab
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-27 20:55:03 +0200

    Update functionoverloading.cpp

--- Theory/Intermediate/functionoverloading.cpp
+++ Theory/Intermediate/functionoverloading.cpp
@@ -1,42 +1,40 @@
 #include<iostream>
 
-
-
-
-
-
-//prototype
+// Prototypes
+// Function overloading: We declare multiple functions with the same name (`show`),
+// but different parameter types (int, char, float).
 void show(int number);
 void show(char character);
 void show(float floating_point_number);
 
+int main() {
+    // Declare three variables of different types: int, char, and float
+    int n = 1;           // Integer
+    char c = 's';        // Character
+    float f = 3.14;      // Floating-point number
 
+    // Call the overloaded function `show`. Based on the argument's data type,
+    // the compiler will decide which version of `show` to call.
+    show(n);    // Calls the version of `show` that takes an int
+    show(f);    // Calls the version of `show` that takes a float
+    show(c);    // Calls the version of `show` that takes a char
 
-int main(){
-	int n = 1;
-	char c = 's';
-	float f = 3.14;
-	//now with the same name we can pass different argument and get a different response based on the argument
-	show(n);
-	show(f);
-	show(c);
-	
-	
-	
-	return 0;
-}
-//sometimes we need our function to do something specifical depending on the parameter passed but we dont want to 
-//take care of every case in a single block we can overlod that function
-//we basically define the same function name for different use cases
-
-void show(int number){
-	std::cout<<number<<" is a number"<<std::endl;
+    return 0;
 }
 
-void show(char character){
-	std::cout<<character<<" is a character"<<std::endl;
+// Function Definitions:
+
+// This version of `show` takes an integer argument and prints a message specific to integers.
+void show(int number) {
+    std::cout << number << " is an integer number" << std::endl;
 }
 
-void show(float floating_point_number){
-	std::cout<<floating_point_number<<" is a floating pooint number"<<std::endl;
+// This version of `show` takes a char argument and prints a message specific to characters.
+void show(char character) {
+    std::cout << character << " is a character" << std::endl;
+}
+
+// This version of `show` takes a float argument and prints a message specific to floating-point numbers.
+void show(float floating_point_number) {
+    std::cout << floating_point_number << " is a floating-point number" << std::endl;
 }
