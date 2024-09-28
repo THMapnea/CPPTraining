7ef55f43c367ec0c414d1577355f47f2cf21b99e
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-28 18:56:01 +0200

    Update classes introduction.cpp

--- Theory/Intermediate/Classes/classes introduction.cpp
+++ Theory/Intermediate/Classes/classes introduction.cpp
@@ -1,56 +1,54 @@
 #include<iostream>
 
+// In C++, we can define more complex data types called classes, which can encapsulate both data (attributes) 
+// and behavior (methods). Unlike C structs, classes allow for access control using specifiers such as 
+// public, private, and protected.
 
+// The concept of classes also introduces Object-Oriented Programming (OOP) paradigms like encapsulation,
+// which hides internal details and exposes only the necessary interfaces to the outside world.
 
-//sometimes it can be useful to define morte complex data types that can also perform some action
-//this is actually already possible in c by encapsulating function in structure but in c++ this approch is much more 
-//precise and studied introducing the concept of the class that implements a specifier allowing to give access to the class only
-//to a specific part of the code
+// Defining a class `User` to represent a user in the system:
+class User {
+    // Public members can be accessed from any part of the code, even outside the class.
+public:
+    std::string user_name;   // This represents the username.
+    std::string user_email;  // This stores the user's email.
 
+    // Private members can only be accessed by methods within the class itself or by "friend" classes/functions.
+private:
+    std::string user_password;  // This stores the user's password, which is kept private and secure.
 
-//data
-//let's say that we want to represent a user on our platform instead of defining a structure we can define a class
-class User{
-	//with the keyword public we allow any part or other class of the code to access it
-	public:
-		std::string user_name;
-		std::string user_email;
-	
-	//private parameters can only be accessed by the same class or their friends
-	private:
-		std::string user_password;
-		
-	//protected attributes can only be accessed by same classes derived classes and friends
-	protected:
-		std::string friends[100];
-	
-	//we can also define function for our classes
-	public:
-		void show(int passed_value){
-			std::cout<<"the email of the account number: "<<passed_value<<" is "<<user_email<<" and the account associated is: "<<user_name<<std::endl;
-		}
-	
+    // Protected members can be accessed by the class itself, derived classes, or "friends."
+protected:
+    std::string friends[100];  // Protected array to store the user's friends.
+
+    // Public methods that can perform actions on the class data.
+public:
+    // A method that prints user information. It accepts an integer parameter as an identifier or value to print.
+    void show(int passed_value) {
+        std::cout << "The email of account number " << passed_value << " is " 
+                  << user_email << " and the associated username is: " << user_name << std::endl;
+    }
+
+    // We could also define additional methods to interact with or modify private/protected data.
 };
 
+// The `main` function is the entry point of the program.
+int main() {
+    // Creating a new instance of the `User` class.
+    User user_1;
 
+    // Since `user_name` and `user_email` are public members, we can directly access and modify them.
+    user_1.user_name = "pino69";  // Assigning a username.
+    user_1.user_email = "pinocalamarino@gmail.com";  // Assigning an email.
 
+    // Calling the `show` method, passing 12 as the `passed_value` parameter.
+    user_1.show(12);
 
+    // NOTE: Private or protected members like `user_password` or `friends` cannot be accessed directly outside the class.
+    // Uncommenting the following lines will cause a compilation error:
+    // user_1.user_password = "secret";  // ERROR: 'user_password' is private.
+    // user_1.friends[0] = "friend1";    // ERROR: 'friends' is protected.
 
-
-
-int main(){
-	//creating a new class consist in instancing teh class/object
-	User user_1;
-	
-	//then from here just like we did with the struct we can initialize it's value
-	user_1.user_name = "pino69";
-	user_1.user_email = "pinocalamarino@gmail.com";
-	user_1.show(12);
-	
-	//as you can see we cannot access private or protected members of the function later we will see how to handle that part of the class
-	
-	
-	
-	
-	return 0;
+    return 0;
 }
