ad81fccb1bfb0663bf9a0d682fc326b02461313c
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-28 18:54:52 +0200

    Update sourcefile.cpp

--- Theory/Intermediate/Code Splitting/sourcefile.cpp
+++ Theory/Intermediate/Code Splitting/sourcefile.cpp
@@ -1,13 +1,19 @@
-//include the compiled header
+// Including the custom header file "header.h".
+// This will bring in the definition of the `Person` struct from the header file.
 #include "header.h"
-#include<iostream>
 
+#include <iostream>  // Including iostream again, but it won't cause any issues since iostream also uses include guards.
 
+int main() {
+    // We can now create an instance of `Person` which is defined in the header file.
+    Person p;
 
-int main(){
-	//as you can see we can call the struct defined in our header file
-	Person p;
-	
-	
-	return 0;
+    // Assigning values to the members of the `Person` struct.
+    p.age = 25;
+    p.name = "John Doe";
+
+    // Outputting the values stored in the `Person` struct.
+    std::cout << "Person Name: " << p.name << ", Age: " << p.age << std::endl;
+
+    return 0;
 }
