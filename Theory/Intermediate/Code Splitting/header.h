c7f99e2415a02db13b61318109326ce4dc6d19fc
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-28 18:54:32 +0200

    Update header.h

--- Theory/Intermediate/Code Splitting/header.h
+++ Theory/Intermediate/Code Splitting/header.h
@@ -1,13 +1,17 @@
-//this line is called guard and check to avoid double definition in the codebase
-#ifndef SOURCEFILE_H
-#define SOURCEFILE_H
-#include<iostream>
+// The following line is a header guard to prevent multiple inclusions of the same header file.
+// If this file has already been included, the symbol `HEADER_H` will be defined,
+// and the preprocessor will skip the content of this file.
+#ifndef HEADER_H
+#define HEADER_H
 
-struct Person{
-	int age;
-	std::string name;
+#include <iostream>  // Including necessary library for std::string and std::cout
+
+// Defining a struct named 'Person'.
+// The struct contains two members: an integer 'age' and a string 'name'.
+struct Person {
+    int age;
+    std::string name;
 };
 
-
-//this is the end of the guarded block
+// End of the header guard. It ensures that the header's content is included only once in the compilation unit.
 #endif
