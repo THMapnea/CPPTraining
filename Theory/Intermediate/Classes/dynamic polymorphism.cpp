fada4d6a633c29f8f1484075f247cd78a954b86f
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-28 18:59:15 +0200

    Update dynamic polymorphism.cpp

--- Theory/Intermediate/Classes/dynamic polymorphism.cpp
+++ Theory/Intermediate/Classes/dynamic polymorphism.cpp
@@ -1,70 +1,70 @@
 #include<iostream>
 
-
-//data
-
-//we define our parent class that contains one method
-class Shape{
-	public:
-		virtual double area() const{
-			return 0;
-		}
+// Parent class `Shape` that contains a virtual method `area`.
+// The method is marked `virtual` to allow derived classes (like Circle and Rectangle) to override it.
+class Shape {
+public:
+    // Virtual method that will be overridden by child classes to calculate the area of specific shapes.
+    virtual double area() const {
+        return 0;  // Default implementation returns 0, as a generic "Shape" has no area.
+    }
 };
 
-//we define the first child class
-class Circle : public Shape{
-	//we create a private field
-	private:
-		double radius;
-	
-	//we override the parent function
-	public:
-    	double area() const override {
-			return 3.14 * radius * radius;
-	}
-	
-	//we define a constructor
-	Circle(double r) : radius(r){}
-	
+// First child class `Circle` that inherits from `Shape`.
+class Circle : public Shape {
+    // Private field to store the radius of the circle.
+private:
+    double radius;
+
+    // Public method to override the `area()` function from the `Shape` class.
+public:
+    // Overrides the parent `Shape` class's `area()` method to compute the area of a circle.
+    double area() const override {
+        return 3.14 * radius * radius;  // Area of a circle formula: π * r^2
+    }
+
+    // Constructor that initializes the circle with a specific radius.
+    Circle(double r) : radius(r) {}
 };
 
+// Second child class `Rectangle` that also inherits from `Shape`.
+class Rectangle : public Shape {
+    // Private fields to store the dimensions (height and width) of the rectangle.
+private:
+    double height;
+    double width;
 
-//then we define the second child class
-class Rectangle : public Shape{
-	//private fields
-	private:
-		double height;
-		double width;
-	
-	//function overriding
-	public:
-		double area() const override{
-			return width*height;
-		}
-	
-	//constructor
-	Rectangle(double w, double h) : width(w),height(h){}
+    // Public method to override the `area()` function from the `Shape` class.
+public:
+    // Overrides the parent `Shape` class's `area()` method to compute the area of a rectangle.
+    double area() const override {
+        return width * height;  // Area of a rectangle formula: width * height
+    }
+
+    // Constructor that initializes the rectangle with specific dimensions.
+    Rectangle(double w, double h) : width(w), height(h) {}
 };
 
+int main() {
+    // Creating two objects: a Circle with radius 3, and a Rectangle with width 3 and height 5.
+    Circle c(3);
+    Rectangle r(3, 5);
+    
+    // Using a pointer of type `Shape*` to reference different shape objects.
+    // First, we point to the `Circle` object.
+    Shape* shape_pointer = &c;
 
+    // When we call `area()` using the `shape_pointer`, the Circle's area function is called due to polymorphism.
+    std::cout << "The circle area is: " << shape_pointer->area() << std::endl;
+    
+    // Now, we reassign the pointer to point to the `Rectangle` object.
+    shape_pointer = &r;
 
-
-int main(){
-	//let's instance two object
-	Circle c(3);
-	Rectangle r(3, 5);
-	
-	//now we create a pointers to the Shape class that will point to one of the two object
-	Shape* shape_pointer = &c;
-	
-	//we can now use the shape pointer and execute the area method and we will get the method defined in the referenced child shape
-	std::cout<<"the circle area is: "<<shape_pointer->area()<<std::endl;
-	
-	//if we reassign the pointer to the rectangle the method area will return the rectangle function
-	shape_pointer = &r;
-	std::cout<<"the area of the rectangle is: "<<shape_pointer->area()<<std::endl;
-	
-	//this can be useful for cleaner code and easier management since we will only be calling the shape_pointer and not different object
-	
-	return 0;
+    // Calling `area()` again will now invoke the Rectangle's overridden `area()` method.
+    std::cout << "The area of the rectangle is: " << shape_pointer->area() << std::endl;
+    
+    // This demonstrates polymorphism: the ability to call the same method (`area()`)
+    // on different types of objects (Circle, Rectangle) and get the correct behavior based on the actual object type.
+    
+    return 0;
 }
