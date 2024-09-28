cfb0afd7b81d113c2a5b35c5c0b945bbfec3cd5b
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-28 19:01:09 +0200

    Update polymorphism.cpp

--- Theory/Intermediate/Classes/polymorphism.cpp
+++ Theory/Intermediate/Classes/polymorphism.cpp
@@ -1,41 +1,42 @@
 #include<iostream>
 
-
-
-//data
-class Animal{
-	public:
-		//the virtual keyword allows for overriding of the function
-		virtual void makeSound(){
-			std::cout<<"SOUND FX"<<std::endl;
-		}
+// Base class `Animal` with a virtual function `makeSound`.
+// The `virtual` keyword allows derived classes (like `Duck` and `Crocodile`) to override this function.
+class Animal {
+public:
+    // The `makeSound` method is virtual, meaning it can be redefined in derived classes.
+    virtual void makeSound() {
+        std::cout << "SOUND FX" << std::endl;  // Default sound for a generic animal.
+    }
 };
 
-
-//we can define child classes that can override and modify the parent class based on their necessity
-class Duck : public Animal{
-	public:
-		void makeSound() override{
-			std::cout<<"quack quack"<<std::endl;
-		}
+// Derived class `Duck` inherits from `Animal` and overrides the `makeSound` method.
+class Duck : public Animal {
+public:
+    // Overriding the `makeSound` method to provide a custom sound for `Duck`.
+    void makeSound() override {
+        std::cout << "quack quack" << std::endl;  // Specific sound for a duck.
+    }
 };
 
-class Crocodile : public Animal{
-	public:
-		void makeSound() override{
-			std::cout<<"nobody knows"<<std::endl;
-		}
+// Derived class `Crocodile` also inherits from `Animal` and overrides the `makeSound` method.
+class Crocodile : public Animal {
+public:
+    // Overriding the `makeSound` method to provide a custom sound for `Crocodile`.
+    void makeSound() override {
+        std::cout << "nobody knows" << std::endl;  // Specific "sound" for a crocodile.
+    }
 };
 
-
-int main(){
-	Duck my_duck;
-	Crocodile my_crocodile;
-	//we have called the same function name but it behaved differently depending on the class
-	my_duck.makeSound();
-	my_crocodile.makeSound();
-	
-	
-	
-	return 0;
+int main() {
+    // Create objects for both the `Duck` and `Crocodile` classes.
+    Duck my_duck;
+    Crocodile my_crocodile;
+    
+    // Even though the `makeSound` method has the same name, the correct version is called
+    // based on the object type (`Duck` or `Crocodile`), due to method overriding.
+    my_duck.makeSound();       // Calls `makeSound()` from the `Duck` class.
+    my_crocodile.makeSound();  // Calls `makeSound()` from the `Crocodile` class.
+    
+    return 0;
 }
