5d9aa32514a1667ee18cf4eaebbfcb7a7af782e3
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-29 17:07:32 +0200

    Update 1108.cpp

--- leetCode/Easy/1108/1108.cpp
+++ leetCode/Easy/1108/1108.cpp
@@ -1,35 +1,35 @@
 #include<iostream>
 
-
-
-//data
+// Data: We have an IP address string.
 std::string address = "1.1.1.1";
 
-//prototype
+// Prototype for the function that modifies the IP address.
 std::string solution(std::string address);
 
+int main() {
+    // Call the solution function and print the result.
+    std::cout << solution(address) << std::endl;
 
-int main(){
-	
-	std::cout<<solution(address)<<std::endl;
-	
-	return 0;
+    return 0;
 }
 
-std::string solution(std::string address){
-    //create a new variable
+// This function takes an IP address and replaces each dot (.) with "[.]".
+std::string solution(std::string address) {
+    // Create a new string to hold the result.
     std::string result;
     
-    //loop trough every character
-    for(char c : address){
-    	
-        //if we find the point we add the new sequence else we assign the normal value
-        if(c == '.'){
+    // Loop through each character in the input IP address string.
+    for (char c : address) {
+        // If the character is a dot, append "[.]" to the result.
+        if (c == '.') {
             result += "[.]";
-        }else{
+        } 
+        // Otherwise, append the character itself to the result.
+        else {
             result += c;
         }
-        
     }
+    
+    // Return the modified string with the replaced dots.
     return result;
 }
