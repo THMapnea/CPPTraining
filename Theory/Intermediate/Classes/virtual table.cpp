27e0472babf670ea794e9efe9a1626a5bf6e5165
Thomas Mario <thmapneadesigner@gmail.com>
2024-09-29 17:05:24 +0200

    Update virtual table.cpp

--- Theory/Intermediate/Classes/virtual table.cpp
+++ Theory/Intermediate/Classes/virtual table.cpp
@@ -1,45 +1,49 @@
 #include<iostream>
 
+// Base class with virtual functions
+class Base {
+public:
+    // Virtual function that can be overridden in derived classes
+    virtual void f1() {
+        std::cout << "base::function1" << std::endl;
+    }
 
-
-//we create a class with virtual functions
-class Base{
-	public:
-		virtual void f1(){
-			std::cout<<"base::function1"<<std::endl;
-		}
-		virtual void f2(){
-			std::cout<<"base::function2"<<std::endl;
-		}
+    // Another virtual function, which can be overridden or left as is in derived classes
+    virtual void f2() {
+        std::cout << "base::function2" << std::endl;
+    }
 };
 
+// Derived class that inherits from `Base` and overrides `f1`
+class Derived : public Base {
+public:
+    // Override the `f1` method from the `Base` class
+    void f1() override {
+        std::cout << "derived::function1" << std::endl;
+    }
 
-//we create a child class with one of the previous function override and a normal one
-class Derived : public Base{
-	public:
-		void f1() override{
-			std::cout<<"derived::function1"<<std::endl;	
-		}
-		void f3(){
-			std::cout<<"derived::function3"<<std::endl;
-		}
+    // Introduce a new function `f3` specific to `Derived`
+    void f3() {
+        std::cout << "derived::function3" << std::endl;
+    }
 };
 
-int main(){
-	
-	//we can create  apointer to the base and assign it to a child class object
-	Base* obj = new Derived();
-	
-	//as we can see we are able only to call the function of the base 
-	//but with the second function being the one defined in the child class
-	obj->f1();
-	obj->f2();
-	
-	//we can now delete the object
-	delete obj;
-	
-	//this happen sinec when we create an object the pointer to the object points to the virtual table containig the definition of the functions
-	//that are then overridden and modyfied when the derived class is called
-	
-	return 0;
+int main() {
+    // Create a pointer to the `Base` class, but assign it an object of the `Derived` class.
+    Base* obj = new Derived();
+    
+    // Call functions using the `Base` pointer.
+    // `f1()` is overridden in the `Derived` class, so it will call `Derived::f1()`.
+    obj->f1();  // Output: "derived::function1"
+    
+    // `f2()` is not overridden in `Derived`, so it will call the `Base` version.
+    obj->f2();  // Output: "base::function2"
+    
+    // Note: Even though the `Derived` class has a `f3()` function, we cannot call it
+    // using a pointer to the `Base` class, because `f3()` is not defined in `Base`.
+
+    // Free the dynamically allocated memory
+    delete obj;
+
+    return 0;
 }
