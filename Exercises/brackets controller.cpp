35b02627aa763cbf9ead823b49c594ee10155bfe
Thomas Mario <thmapneadesigner@gmail.com>
2024-10-04 19:08:33 +0200

    Update brackets controller.cpp

--- Exercises/brackets controller.cpp
+++ Exercises/brackets controller.cpp
@@ -7,7 +7,7 @@
 
 
 //data
-std::vector<std::string> data = {"{([()])}"};
+std::string data = {"{([()])}"};
 
 
 
@@ -16,8 +16,28 @@ std::vector<std::string> data = {"{([()])}"};
 int main(){
 	
 	
+	int circular_counter = 0;
+	int square_counter = 0;
+	int curly_counter = 0;
 	
+	for(auto& element : data){
+		if(element == '(' || element == ')'){
+			circular_counter += 1;
+		}else if(element == '[' || element == ']'){
+			square_counter += 1;
+		}else if(element == '{' || element == '}'){
+			curly_counter += 1;
+		}
+		
+	}
 	
+	if(circular_counter % 2 == 0 && square_counter % 2 == 0 && curly_counter % 2 == 0){
+		std::cout<<"correct number of brackets"<<std::endl;
+	}else{
+		std::cout<<"some brackets are incomplete"<<std::endl;
+	}
+	
+
 	
 	return 0;
 }
