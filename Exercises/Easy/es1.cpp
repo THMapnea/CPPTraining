fc1bc3081ecbe3ac530182c132e1edb1e36913bc
Thomas Mario <thmapneadesigner@gmail.com>
2024-10-21 19:02:02 +0200

    Update es1.cpp

--- Exercises/Easy/es1.cpp
+++ Exercises/Easy/es1.cpp
@@ -12,17 +12,17 @@ int main() {
     int x;
 
     do {
-        std::cout << "Insert a positive number: ";
-        std::cin >> x;
+        std::cout<<"Insert a positive number: ";
+        std::cin>>x;
 
         // Check if the input is invalid (e.g., not a number)
         if (std::cin.fail()) {
-            std::cout << "You didn't insert a number." << std::endl;
+            std::cout<<"You didn't insert a number."<<std::endl;
             break;
         }
     } while (x < 0);
 
-    std::cout << "You entered a valid positive number: " << x << std::endl;
+    std::cout<<"You entered a valid positive number: "<<x<< std::endl;
 
     return 0;
 }
