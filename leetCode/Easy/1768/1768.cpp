3a295282ffcdf680530630303aa0d2e181b5c6ff
Thomas Mario <thmapneadesigner@gmail.com>
2024-12-07 22:45:35 +0100

    Update 1768.cpp

--- leetCode/Easy/1768/1768.cpp
+++ leetCode/Easy/1768/1768.cpp
@@ -3,7 +3,7 @@
 
 
 
-class Solution {
+class Solution1 {
 public:
     // Function to merge two strings alternately
     string mergeAlternately(string word1, string word2) {
@@ -27,6 +27,35 @@ public:
             else if (i % 2 != 0 && k != word2.size()) {
                 result.push_back(word2[k]); // Append character from word2
                 k++; // Move the pointer for word2 to the next character
+            }
+        }
+        
+        // Return the final merged string
+        return result;
+    }
+};
+
+
+class Solution2 {
+public:
+    // Function to merge two strings alternately
+    string mergeAlternately(string word1, string word2) {
+        // Initialize two pointers to track the current index for word1 and word2
+        int i = 0, j = 0;
+        
+        // Initialize an empty result string to store the merged result
+        string result = "";
+        
+        // Continue merging until all characters from either word1 or word2 are consumed
+        while (i < word1.size() || j < word2.size()) {
+            // If there are remaining characters in word1, append the next character to result
+            if (i < word1.size()) {
+                result += word1[i++]; // Append character from word1 and move pointer i forward
+            }
+            
+            // If there are remaining characters in word2, append the next character to result
+            if (j < word2.size()) {
+                result += word2[j++]; // Append character from word2 and move pointer j forward
             }
         }
         
